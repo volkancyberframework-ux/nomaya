156ada7a96b1aef95f44d28a8e4bae899b249c3c
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-10-17 12:05:43 +0200

    Update: templates, TR dates, stepper tweaks, contact card, settings

--- nomaya/settings.py
+++ nomaya/settings.py
@@ -9,11 +9,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-@dqu-@telwen(6x(q00gnsg_vk)()r&+atofj-6yyjqopy!s9+")
 DEBUG = os.environ.get("DEBUG", "1") == "1"   # local: 1, prod: 0
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["nomaya.co", "www.nomaya.co", "nomaya.onrender.com"]
+
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.onrender.com",
     "https://nomaya.co",
+    "https://www.nomaya.co",
 ]
+SECURE_SSL_REDIRECT = True           # Render’da HTTPS aktifse güzel olur
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
 
 # --- Auth yönlendirmeleri ---
 LOGIN_URL = "sign_in"
