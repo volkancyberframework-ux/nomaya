9d4b1e45cbcff190b45f949cbb6e01599dad52af
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-11-04 18:56:52 +0100

    Update: templates, TR dates, stepper tweaks, contact card, settings

--- nomaya/urls.py
+++ nomaya/urls.py
@@ -1,10 +1,11 @@
 # project/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
