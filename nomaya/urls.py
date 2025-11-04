f4eba205f16792f544e0712578ecf5ae86b89c3a
volkanguler <volkanguler@MacBook-Pro-van-volkanguler.local>
2025-11-04 18:35:44 +0100

    Update: templates, TR dates, stepper tweaks, contact card, settings

--- nomaya/urls.py
+++ nomaya/urls.py
@@ -6,3 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),  # TÜM app rotaları core.urls'te
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # WhiteNoise static dosyaları servis eder; media’yı biz servis edeceğiz
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
